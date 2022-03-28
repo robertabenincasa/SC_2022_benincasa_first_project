c114f7998864f99d10d17a4350972fdcfc871cad
robertabenincasa <102039144+robertabenincasa@users.noreply.github.com>
2022-03-28 20:15:39 +0200

    Update test_cellularautomata.py
    Boundary conditions as a separate function.

--- test_cellularautomata.py
+++ test_cellularautomata.py
@@ -50,16 +50,18 @@ nsteps=10
 
 seed(7)
 def generate_state():
-"""This function generates the initial state as a string having n elements composed of . 
-and only one 0, i.e. only dead cells except for one.
-The random function is used in order to have the 0 in a random position.
-If one wants to maintain the same position than the seed must be fixed above."""
+    "This function generates the initial state as a string having n elements composed of "
+    "The random function is used in order to have the 0 in a random position."
+    "If one wants to maintain the same position than the seed must be fixed above."
     state = ['.']*(n-1)
     state.insert(randint(0,n-1),'0')
     initial_state=''.join(state)
     return initial_state
 
 def boundcond(stato):
+    """This function applies the boundary conditions that one chooses to adopt.
+    The boundaries can be reflective, periodic or constant. 
+    It takes as input the state to be evolved. """
     if bc=='const':
         status=''.join(('.',stato,'.')) #constant boundaries
     elif bc=='refl':
@@ -71,10 +73,10 @@ def boundcond(stato):
     return status
 
 def evolve(stato):
-""" This function perfoms the evolution of each state following the chosen rule and exploiting the boundary conditions that one chooses to adopt.
+    """ This function perfoms the evolution of each state following the chosen rule.
 
-The rule can be rule 30, 90, 110 or 184. The boundaries can be reflective, periodic or constant. 
-It takes as input the state to be evolved. """
+    The rule can be rule 30, 90, 110 or 184. 
+    It takes as input the state to be evolved. """
 
     evoluto=[''] * n
     status=boundcond(stato)
@@ -88,7 +90,7 @@ It takes as input the state to be evolved. """
     return new_state
 
 def simulation(nsteps):
-""" This function performs the simulation by evolving the system from the initial state for nsteps."""
+    """ This function performs the simulation by evolving the system from the initial state for nsteps."""
     initial_state = generate_state()
     states_seq = [initial_state]
     print(initial_state)
@@ -105,24 +107,24 @@ states_seq=simulation(nsteps)
 ########################################################
 
 def test_generation_valid_state():
-""" This function tests that the initial state is valid.
+    """ This function tests that the initial state is valid.
 
-GIVEN: the initial state from generate.state()
-THEN: it results in a string composed of . and 0. """
+    GIVEN: the initial state from generate.state()
+    THEN: it results in a string composed of . and 0. """
     state = generate_state()
     assert set(state) == {'.', '0'}
     
 
 def test_generation_single_alive():
-""" This function tests if the initial state is a string composed of . and only one 0.
-GIVEN: the initial state from generate.state()
-THEN: it produces a string formed of . and only one 0.""" 
+    """ This function tests if the initial state is a string composed of . and only one 0.
+    GIVEN: the initial state from generate.state()
+    THEN: it produces a string formed of . and only one 0.""" 
     state = generate_state()
     num_of_0 = sum(1 for i in state if i=='0')
     assert num_of_0 == 1
     
 def test_evolution_valid_state():
-""" This function tests that the evolve function returns valid states when used.
+    """ This function tests that the evolve function returns valid states when used.
     
     GIVEN: a valid state of my simulation
     WHEN: I apply to it the evolve function
@@ -132,7 +134,7 @@ def test_evolution_valid_state():
         assert set(state) == {'.', '0'}
         
 def test_evolution_valid_length():
-""" This function tests that the evolve function returns a state of the same length of the initial one.
+    """ This function tests that the evolve function returns a state of the same length of the initial one.
     
     GIVEN: a valid state of my simulation
     WHEN: I apply to it the evolve function
@@ -142,7 +144,7 @@ def test_evolution_valid_length():
         assert len(state)==len(generate_state())
 
 def test_valid_bcs():
-""" This function tests that the selected boundary conditions are correct.
+    """ This function tests that the selected boundary conditions are correct.
     
     GIVEN: a string for the value of the boundary conditions
     THEN: it must be one among the following.
